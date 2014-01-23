0550d3001fb2dd6010e527dbdb178f75afeef1a9
hertsch <ralf.hertsch@phpmanufaktur.de>
2014-01-23 02:23:31 +0100

    Changed handling for symbolic links

--- Setup/Setup.php
+++ Setup/Setup.php
@@ -26,47 +26,87 @@ class Setup
     {
         // create symlinks for the LIBRARY directories
 
-        $app['filesystem']->symlink(
-            MANUFAKTUR_PATH.'/Library/Library/bootstrap/3.0.3',
-            MANUFAKTUR_PATH.'/Library/Library/bootstrap/latest');
+        if (chdir(MANUFAKTUR_PATH.'/Library/Library/bootstrap')) {
+            $app['filesystem']->symlink('3.0.3', 'latest');
+            $app['monolog']->addDebug('Created SymLink in /Library/Library/bootstrap');
+        }
+        else {
+            $app['monolog']->addDebug('Failed to create SymLink in /Library/Library/bootstrap');
+        }
 
-        $app['filesystem']->symlink(
-            MANUFAKTUR_PATH.'/Library/Library/font-awesome/4.0.3',
-            MANUFAKTUR_PATH.'/Library/Library/font-awesome/latest');
+        if (chdir(MANUFAKTUR_PATH.'/Library/Library/font-awesome')) {
+            $app['filesystem']->symlink('4.0.3', 'latest');
+            $app['monolog']->addDebug('Created SymLink in /Library/Library/font-awesome');
+        }
+        else {
+            $app['monolog']->addDebug('Failed to create SymLink in /Library/Library/font-awesome');
+        }
 
-        $app['filesystem']->symlink(
-            MANUFAKTUR_PATH.'/Library/Library/jquery/jquery/2.0.3',
-            MANUFAKTUR_PATH.'/Library/Library/jquery/jquery/latest');
+        if (chdir(MANUFAKTUR_PATH.'/Library/Library/jquery/jquery')) {
+            $app['filesystem']->symlink('2.0.3', 'latest');
+            $app['monolog']->addDebug('Created SymLink in /Library/Library/jquery/jquery/2.0.3');
+        }
+        else {
+            $app['monolog']->addDebug('Failed to create SymLink in /Library/Library/jquery/jquery/2.0.3');
+        }
 
-        $app['filesystem']->symlink(
-            MANUFAKTUR_PATH.'/Library/Library/jquery/migrate/1.2.1',
-            MANUFAKTUR_PATH.'/Library/Library/jquery/migrate/latest');
+        if (chdir(MANUFAKTUR_PATH.'/Library/Library/jquery/migrate')) {
+            $app['filesystem']->symlink('1.2.1', 'latest');
+            $app['monolog']->addDebug('Created SymLink in /Library/Library/jquery/migrate/1.2.1');
+        }
+        else {
+            $app['monolog']->addDebug('Failed to create SymLink in /Library/Library/jquery/migrate/1.2.1');
+        }
 
-        $app['filesystem']->symlink(
-            MANUFAKTUR_PATH.'/Library/Library/jquery/tagedit/1.5.1',
-            MANUFAKTUR_PATH.'/Library/Library/jquery/tagedit/latest');
+        if (chdir(MANUFAKTUR_PATH.'/Library/Library/jquery/tagedit')) {
+            $app['filesystem']->symlink('1.5.1', 'latest');
+            $app['monolog']->addDebug('Created SymLink in /Library/Library/jquery/tagedit/1.5.1');
+        }
+        else {
+            $app['monolog']->addDebug('Failed to create SymLink in /Library/Library/jquery/tagedit/1.5.1');
+        }
 
-        $app['filesystem']->symlink(
-            MANUFAKTUR_PATH.'/Library/Library/jquery/timepicker/1.4.3',
-            MANUFAKTUR_PATH.'/Library/Library/jquery/timepicker/latest');
+        if (chdir(MANUFAKTUR_PATH.'/Library/Library/jquery/timepicker')) {
+            $app['filesystem']->symlink('1.4.3', 'latest');
+            $app['monolog']->addDebug('Created SymLink in /Library/Library/jquery/timepicker/1.4.3');
+        }
+        else {
+            $app['monolog']->addDebug('Failed to create SymLink in /Library/Library/jquery/timepicker/1.4.3');
+        }
 
-        $app['filesystem']->symlink(
-            MANUFAKTUR_PATH.'/Library/Library/jquery/ui/1.10.3',
-            MANUFAKTUR_PATH.'/Library/Library/jquery/ui/latest');
+        if (chdir(MANUFAKTUR_PATH.'/Library/Library/jquery/ui')) {
+            $app['filesystem']->symlink('1.10.3', 'latest');
+            $app['monolog']->addDebug('Created SymLink in /Library/Library/jquery/ui/1.10.3');
+        }
+        else {
+            $app['monolog']->addDebug('Failed to create SymLink in /Library/Library/jquery/ui/1.10.3');
+        }
 
         // create symlinks for the EXTENSION directories
 
-        $app['filesystem']->symlink(
-            MANUFAKTUR_PATH.'/Library/Extension/htmlpurifier/4.6.0',
-            MANUFAKTUR_PATH.'/Library/Extension/htmlpurifier/latest');
+        if (chdir(MANUFAKTUR_PATH.'/Library/Extension/htmlpurifier')) {
+            $app['filesystem']->symlink('4.6.0', 'latest');
+            $app['monolog']->addDebug('Created SymLink in /Library/Extension/htmlpurifier/4.6.0');
+        }
+        else {
+            $app['monolog']->addDebug('Failed to create SymLink in /Library/Extension/htmlpurifier/4.6.0');
+        }
 
-        $app['filesystem']->symlink(
-            MANUFAKTUR_PATH.'/Library/Extension/carbon/1.8.0',
-            MANUFAKTUR_PATH.'/Library/Extension/carbon/latest');
+        if (chdir(MANUFAKTUR_PATH.'/Library/Extension/carbon')) {
+            $app['filesystem']->symlink('1.8.0', 'latest');
+            $app['monolog']->addDebug('Created SymLink in /Library/Extension/carbon/1.8.0');
+        }
+        else {
+            $app['monolog']->addDebug('Failed to create SymLink in /Library/Extension/carbon/1.8.0');
+        }
 
-        $app['filesystem']->symlink(
-            MANUFAKTUR_PATH.'/Library/Extension/dflydev/markdown/1.0.3',
-            MANUFAKTUR_PATH.'/Library/Extension/dflydev/markdown/latest');
+        if (chdir(MANUFAKTUR_PATH.'/Library/Extension/dflydev/markdown')) {
+            $app['filesystem']->symlink('1.0.3', 'latest');
+            $app['monolog']->addDebug('Created SymLink in /Library/Extension/dflydev/markdown/1.0.3');
+        }
+        else {
+            $app['monolog']->addDebug('Failed to create SymLink in /Library/Extension/dflydev/markdown/1.0.3');
+        }
 
     }
 
